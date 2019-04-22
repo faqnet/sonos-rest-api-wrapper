b5c65cd09a25b4139df32848c60fcac72c06258f
Robin Schoch <robin.schoch@fhnw.ch>
2019-04-22 17:17:26 +0200

    added read me

--- sonos_runner.py
+++ sonos_runner.py
@@ -4,5 +4,6 @@ if __name__ == '__main__':
     sonos = MySonos.from_config('config.json')
     sonos.discover()
     household = sonos.households[0].get_groups_and_players()
+
     #linvingRoom = household.find_group_by_name("Living room")
 
