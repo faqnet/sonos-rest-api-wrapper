cd980cc7ca6a0a9510f40a15d7262af265e5b172
Robin Schoch <robin.schoch@fhnw.ch>
2019-05-02 17:27:36 +0200

    seutp

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='sonos',
+    version='0.0.1',
+    description='a pip-installable package example',
+    license='MIT',
+    packages=['sonos'],
+    author='Robin Schoch',
+    author_email='robin.schoch@fhnw.ch',
+    keywords=['sonos'],
+    url='https://github.com/faqnet/sonos-rest-api-wrapper'
+)
